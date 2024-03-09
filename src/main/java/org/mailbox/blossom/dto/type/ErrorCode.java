1da7cc2270e8a092b36495c367b4c7d70ed11899
HyungJoon <kr.dev.hyungjoon@gmail.com>
2024-03-09 21:31:16 +0900

    feat: 파일 타입 에러

--- src/main/java/org/mailbox/blossom/dto/type/ErrorCode.java
+++ src/main/java/org/mailbox/blossom/dto/type/ErrorCode.java
@@ -20,8 +20,6 @@ public enum ErrorCode {
     NOT_FOUND_ITEM(40405, HttpStatus.NOT_FOUND, "해당 아이템을 찾을 수 없습니다" ),
     NOT_FOUND_LETTER(40406, HttpStatus.NOT_FOUND, "해당 편지가 존재하지 않습니다."),
 
-
-
     // Invalid Argument Error
     MISSING_REQUEST_PARAMETER(40000, HttpStatus.BAD_REQUEST, "필수 요청 파라미터가 누락되었습니다."),
     INVALID_ARGUMENT(40001, HttpStatus.BAD_REQUEST, "요청에 유효하지 않은 인자입니다."),
@@ -29,12 +27,12 @@ public enum ErrorCode {
     INVALID_HEADER_ERROR(40003, HttpStatus.BAD_REQUEST, "유효하지 않은 헤더입니다."),
     MISSING_REQUEST_HEADER(40004, HttpStatus.BAD_REQUEST, "필수 요청 헤더가 누락되었습니다."),
     BAD_REQUEST_PARAMETER(40005, HttpStatus.BAD_REQUEST, "잘못된 요청 파라미터입니다."),
+    UNSUPPORTED_MEDIA_TYPE(40006, HttpStatus.BAD_REQUEST, "지원하지 않는 미디어 타입입니다."),
     BAD_REQUEST_JSON(40006, HttpStatus.BAD_REQUEST, "잘못된 JSON 형식입니다."),
 
     // Access Denied Error
     ACCESS_DENIED(40300, HttpStatus.FORBIDDEN, "접근 권한이 없습니다."),
     NOT_MATCH_USER(40301, HttpStatus.FORBIDDEN, "해당 사용자가 일치하지 않습니다."),
-
 
     // Unauthorized Error
     FAILURE_LOGIN(40100, HttpStatus.UNAUTHORIZED, "잘못된 아이디 또는 비밀번호입니다."),
@@ -53,8 +51,6 @@ public enum ErrorCode {
 
     // External Server Error
     EXTERNAL_SERVER_ERROR(50200, HttpStatus.BAD_GATEWAY, "서버 외부 에러입니다."),
-
-
 
     ;
 
