0f47c9e1bafe12510803abcf905055b2f207c11a
HyungJoon <kr.dev.hyungjoon@gmail.com>
2024-03-09 23:17:09 +0900

    fix: Update LetterDetailDto.java

--- src/main/java/org/mailbox/blossom/dto/request/LetterDetailDto.java
+++ src/main/java/org/mailbox/blossom/dto/request/LetterDetailDto.java
@@ -9,7 +9,7 @@ public record LetterDetailDto(
 
         String receiverId,
 
-        @Size(min = 1, max = 10, message = "Sender name should be less than 10 characters")
+        @Size(max = 10, message = "Sender name should be less than 10 characters")
         String sender,
         @Size(min = 1, max = 200, message = "Letter content should be less than 200 characters")
         String content
